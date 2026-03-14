d0c55a3b6373e03c8703ef98059f6004ccd23303
ArkhamCookie <git@mail.theuntitledproject.dev>
2026-03-14 11:27:10 -0500

    feat: eslint style linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config'
 import globals from 'globals';
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig(
     eslint.configs.recommended,
@@ -16,9 +17,17 @@ export default defineConfig(
         ignores: ['node_modules/'],
     },
     {
+        plugins: {
+            '@stylistic': stylistic,
+        }
+    },
+    {
         rules: {
-            semi: "warn",
+            "semi": "warn",
             "prefer-const": "error",
+            "@stylistic/indent": ["warn", "tab"],
+            "@stylistic/semi": ["warn", "always"],
+            "@stylistic/quotes": ["warn", "double"],
         },
     },
 );
