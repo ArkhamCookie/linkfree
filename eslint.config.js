55262730b46dc69392aa60b9cfca5c68b3be88ab
ArkhamCookie <git@mail.theuntitledproject.dev>
2026-03-14 11:27:35 -0500

    fix: lint and fix file

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,33 @@
-import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config'
-import globals from 'globals';
-import stylistic from '@stylistic/eslint-plugin'
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig(
-    eslint.configs.recommended,
-    {
-        languageOptions: {
-            globals: {
-                ...globals.browser,
-            },
-        },
-    },
-    {
-        files: ['public/js/*.js'],
-        ignores: ['node_modules/'],
-    },
-    {
-        plugins: {
-            '@stylistic': stylistic,
-        }
-    },
-    {
-        rules: {
-            "semi": "warn",
-            "prefer-const": "error",
-            "@stylistic/indent": ["warn", "tab"],
-            "@stylistic/semi": ["warn", "always"],
-            "@stylistic/quotes": ["warn", "double"],
-        },
-    },
+	eslint.configs.recommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+		},
+	},
+	{
+		files: ["public/js/*.js"],
+		ignores: ["node_modules/"],
+	},
+	{
+		plugins: {
+			"@stylistic": stylistic,
+		}
+	},
+	{
+		rules: {
+			"semi": "warn",
+			"prefer-const": "error",
+			"@stylistic/indent": ["warn", "tab"],
+			"@stylistic/semi": ["warn", "always"],
+			"@stylistic/quotes": ["warn", "double"],
+		},
+	},
 );
